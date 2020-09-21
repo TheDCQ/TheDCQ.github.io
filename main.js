b1dfd6c8d084f4d69c6ee9fba052396ed2b00bb2
cainamisir <vlad.cainamisir@gmail.com>
2020-09-21 20:15:10 +0300

    Plati nu merg

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 document.getElementById("SubscribeButton").onclick = function () {
 
-    document.getElementById( 'Section2' ).scrollIntoView();
+    console.log('mishu');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', "http://5.12.105.234:8080/subscribe?mail=" + document.getElementById("Email").value +"&difficulty=medium", false);
+    
+    xhr.send(null);
+    console.log('Done');
 };
